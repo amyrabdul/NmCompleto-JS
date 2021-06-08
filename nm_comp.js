482f35c6341c87392783fa29fc9e93480ed82169
Amyr Abdul <amyrabdul@hotmail>
2021-06-08 19:05:12 -0300

    codificaçao

---
+++ nm_comp.js
@@ -0,0 +1,13 @@
+let nome= prompt("Qual seria o seu Nome");
+
+let sobrenome= prompt("Agora me diga o seu Sobrenome");
+
+let nomecompleto= `Nome Completo: ${nome} ${sobrenome}.`
+
+alert(nomecompleto);
+
+let sobrenome2= sobrenome.toUpperCase()
+
+let nomecompleto2= `Nome de catálogo: ${sobrenome2} ${nome}.`
+
+alert(nomecompleto2);
